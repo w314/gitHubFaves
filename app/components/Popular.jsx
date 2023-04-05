2ec08919bcbc56f55587e796c0f79a7844ed7f43
Piroska Werner <piroska.werner@gmail.com>
2023-04-04 20:04:38 -0500

    feat: Add menu to select languages to display

---
+++ app/components/Popular.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Popular extends React.Component {
+  render() {
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+      <select>
+        {languages.map((language) => (
+          <option key={language} value={language}>
+            {language}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
---
+++ app/index.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import Popular from "./components/Popular";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="light">
+        <div className="container">
+          <Popular />
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
+
+// plugins: [
+//   new HtmlWebpackPlugin({
+//     inject: true,
+//     template: "/app/index.html",
+//     filename: "index.html"
+//   })
+// ]
