f8806f9bf5c674eaba598298dc338d908a49766e
Piroska Werner <piroska.werner@gmail.com>
2023-04-17 20:17:08 -0500

    feat: Add Results of battle

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,18 +1,143 @@
 import React from "react";
+import { battle } from "../util/api";
+import PropTypes from "prop-types";
+
+function Card({ profile }) {
+  const {
+    login,
+    avatar_url,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    location,
+    company,
+  } = profile;
+
+  return (
+    <div className="card bg-light">
+      <header className="split">
+        <div>
+          <h4>
+            <a href={html_url}>{login}</a>
+          </h4>
+          <p>{location || "unknown"}</p>
+        </div>
+        <img
+          className="avatar large"
+          src={avatar_url}
+          alt={`Avatar for ${login}`}
+        />
+      </header>
+      <ul className="stack">
+        <li className="split">
+          <span>Name:</span> <span>{login || "n/a"}</span>
+        </li>
+        <li className="split">
+          <span>Company:</span> <span>{company || "n/a"}</span>
+        </li>
+        <li className="split">
+          <span>Followers:</span> <span>{followers}</span>
+        </li>
+        <li className="split">
+          <span>Following:</span> <span>{following}</span>
+        </li>
+        <li className="split">
+          <span>Repositories:</span> <span>{public_repos}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  profile: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    repositories: PropTypes.number,
+    location: PropTypes.string,
+    company: PropTypes.string,
+  }).isRequired,
+};
 
 export default class Results extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.playerOne = this.playerOne
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    };
+  }
 
-  // }
-
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        console.log("data", players);
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false,
+        });
+        console.log(this.state);
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+        });
+      });
+  }
   render() {
+    const { winner, loser, error, loading } = this.state;
+    console.log(this.state);
+
+    if (loading === true) {
+      return <i>LOADING </i>;
+    }
+
+    if (error) {
+      return <p className="text-center error">{error}</p>;
+    }
     return (
-      <div>
-        Results
+      <main className="animate-in stack main-stack">
+        <div className="spit">
+          <h1>Results</h1>
+        </div>
+        <section className="grid">
+          <article className="results-container">
+            <Card profile={winner.profile} />
+            <p className="results">
+              <span>
+                {winner.score === loser.score ? "Tie" : "Winner"}{" "}
+                {winner.score.toLocaleString()}
+              </span>
+              {winner.score !== loser.score && (
+                <img
+                  src="https://ui.dev/images/certificate.svg"
+                  alt="Certificate"
+                  width={80}
+                />
+              )}
+            </p>
+          </article>
+          <article className="results-container">
+            <Card profile={loser.profile} />
+            <p className="results">
+              <span>
+                {winner.score === loser.score ? "Tie" : "Loser"}{" "}
+                {loser.score.toLocaleString()}
+              </span>
+            </p>
+          </article>
+        </section>
         <pre>{JSON.stringify(this.props, null, 2)}</pre>
-      </div>
+      </main>
     );
   }
 }
--- app/util/api.js
+++ app/util/api.js
@@ -1,18 +1,79 @@
 export function fetchPopularRepos(language) {
-  
   const endpoint = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
   );
 
-  console.log(`in api, endpoint is: ${endpoint}`)
-  
+  console.log(`in api, endpoint is: ${endpoint}`);
+
   // fetch has to be on same line as return or return needs () otherwise type error is thrown
   return fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
-      if(!data.items) {
-        throw new Error(data.message)
+      if (!data.items) {
+        throw new Error(data.message);
       }
-      return data.items
-    } )
+      return data.items;
+    });
+}
+
+function getErrorMsg(message, username) {
+  if (message === "Not Found") {
+    return `${username} does not exist`;
+  }
+
+  return message;
+}
+
+function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        //error
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+      console.log(profile);
+      return profile;
+    });
+}
+
+function getRepos(username) {
+  return fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        //error
+        throw new Error(getErrorMsg(repos.message, username));
+      }
+      return repos;
+    });
+}
+
+function getStarCount(repos) {
+  return repos.reduce((total, { stargazers_count }) => {
+    return total + stargazers_count;
+  }, 0);
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarCount(repos);
+}
+
+function getUserData(player) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    sortPlayers
+  );
 }
